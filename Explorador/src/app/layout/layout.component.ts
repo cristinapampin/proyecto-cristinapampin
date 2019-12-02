753557cc679d3f017ff1f7dd0d27c0ef651f3c02
Cris Pampin <cristina.rpampin@gmail.com>
2019-12-02 18:26:09 +0100

    ng g @angular/material:nav layout/nav-shell

---
+++ Explorador/src/app/layout/layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'abanca-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.css']
+})
+export class LayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- Explorador/src/app/layout/layout.module.ts
+++ Explorador/src/app/layout/layout.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout.component';
+import { NavShellComponent } from './nav-shell/nav-shell.component';
+import { LayoutModule as lmm} from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LayoutComponent, NavShellComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    lmm,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
+  exports:[NavShellComponent]
 })
 export class LayoutModule { }
