46fc92c048a91c0b54e8186233f758d028af701a
Cristina Rivas Pampin <cristina.rivaspampin@plexus.local>
2020-03-24 13:54:14 +0100

    spinner done

--- Explorador/src/app/home/home.module.ts
+++ Explorador/src/app/home/home.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, SharedModule]
+  imports: [CommonModule, HomeRoutingModule, SharedModule, MatProgressSpinnerModule]
 })
 export class HomeModule {}
